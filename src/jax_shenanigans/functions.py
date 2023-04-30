3266cfc0ff47dd1d0e720909af928f460041d9da
Alejandro <alejandrobarongarcia10@gmail.com>
2023-04-30 10:25:57 +0200

    Type hints

--- src/jax_shenanigans/functions.py
+++ src/jax_shenanigans/functions.py
@@ -1,9 +1,11 @@
-def MSE(B, X, y):
+from .utils.typing import array
+
+def MSE(w: array, X:array, y:array):
     """Computes Mean Squared Error"""
-    se2 = (y - X @ B) ** 2 # quadratic errors
+    se2 = (y - X @ w) ** 2 # quadratic errors
     return se2.mean()
 
-def MSE_grad(w, X, y):
+def MSE_grad(w:array, X:array, y:array):
     "Explicit formulation for the gradient of the MSE"
     l = len(X)
     return (1/l) * 2 * (X.T @ (X @ w - y))
---
+++ src/jax_shenanigans/utils/typing.py
@@ -0,0 +1,5 @@
+from typing import Union
+from jax.numpy import ndarray as jax_ndarray
+from numpy import ndarray
+
+array = Union[ndarray, jax_ndarray]
