190bfcfb3d6326eec2678d36004a060df23be248
Alejandro <alejandrobarongarcia10@gmail.com>
2023-05-29 20:47:24 +0200

    Dag tensor hints

--- src/jax_shenanigans/core/_Tensor.py
+++ src/jax_shenanigans/core/_Tensor.py
@@ -12,34 +12,29 @@ class Tensor(DAGNode):
     def __init__(self, x: ndarray, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._x = x
+        self.gradient = None
 
     def __repr__(self) -> str:
         return f"jax_shenanigans.Tensor({self._x}, id={self.id})"
 
 
-def _binary_op_wrap(op_name):
+def _binary_op_wrap(op_name: str):
     op = getattr(DeviceArray, op_name)
 
     @wraps(op)
-    def opwrap(self, other: Any):
-        y = other
-        parents = []
-        id = None
+    def opwrap(self: Tensor, other: Any):
         if isinstance(other, Tensor):
-            y = other._x
-            parents = other.parents
-            id = other.id
-        y_tensor = Tensor(y, parents=parents, id=id)
-        return Tensor(op(self._x, y), parents=[self, y_tensor])
+            return Tensor(op(self._x, other._x), parents=[self, other])
+        return Tensor(op(self._x, other), parents=[self, Tensor(other)])
 
     return opwrap
 
 
-def _unary_op_wrap(op_name):
+def _unary_op_wrap(op_name: str):
     op = getattr(DeviceArray, op_name)
 
     @wraps(op)
-    def opwrap(self):
+    def opwrap(self: Tensor):
         return Tensor(op(self._x), parents=[self])
 
     return opwrap
